3191381b7908be4adadd1b15a6f19c204e7e41ae
Camay <camayall_95@yahoo.com>
2017-03-14 03:46:53 +0000

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-from app import views
+app.config['SECRET_KEY'] = "$$tarz"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:password@localhost/project1"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# Location where image uploads will be stored
+UPLOAD_FOLDER = './app/static/uploads'
+
+# Allowed image upload extensions
+#IMAGES = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
+db = SQLAlchemy(app)
+
+app.config.from_object(__name__)
+from app import views, models
